248c1514ffe3c23485e34c6efa6cfcb621d47f43
letchik063 <letchik063@gmail.com>
2024-03-25 20:33:13 +0200

    added lecture 22, 23

---
+++ exercises/lecture-22/app.js
@@ -0,0 +1,74 @@
+// task 1
+
+const classes = ['first', 'second', 'third', 'fourth'];
+h1 = document.getElementsByTagName('h1')
+console.log(h1);
+console.log(typeof h1);
+console.log(h1.length);
+for (i = 0; i < h1.length; i++) {
+    console.log(h1[i]); 
+}
+
+// task 2
+
+let p1 = document.querySelector('#p1');
+console.log(p1);
+p1.style.backgroundColor = "gold";
+
+// task 3
+
+let p2 = document.querySelector('#p2');
+console.log(p2);
+p2.style.cssText = "background-color:gold; color: blue; font-size: 2rem;"
+
+// task 4
+
+let p3 = document.querySelector('#p3');
+console.log(p3);
+p3.classList.add('third');
+
+// task 5
+
+let p4 = document.querySelector('#p4');
+console.log(p4);
+p4.classList.add('fourth', 'border');
+
+// task 6
+
+let containers = document.querySelectorAll('.container');
+for (let i = 0; i < containers.length; i++) {
+    console.log(containers[i].firstElementChild);
+}
+
+// task 7
+
+let containers1 = document.querySelectorAll('.container');
+for (let i = 0; i < containers.length; i++) {
+    console.log(containers[i].firstElementChild.textContent);
+}
+
+// task 8
+
+let headers = document.querySelectorAll('.container header');
+const classes = ['first', 'second', 'third', 'fourth'];
+console.log(headers)
+for (let i = 0; i < headers.length; i++) {
+    switch (i) {
+        case 0:
+            break;
+        case 1:
+            headers[i].innerHTML = "<h2 class='title second'>Second Title</h2>";
+            break;
+        case 2:
+            headers[i].innerHTML = "<h3 class='title third'>Third Title</h3>";
+            break;
+        case 3:
+            const oldId = headers[i].id;
+            const oldClass = headers[i].className;
+            headers[i].outerHTML = `<h4 class="title fourth" id="${oldId}" class="${oldClass}">Fourth Title</h4>`;
+            break;
+        default:
+            break;
+    }
+    headers[i].classList.add(classes[i]);
+}
---
+++ exercises/lecture-23/app.js
@@ -0,0 +1,98 @@
+const alert = document.querySelector('.alert');
+
+// task 1
+
+let btn = document.querySelector('.btn-primary');
+btn.addEventListener('onclick', () => {
+    alert.classList.add('alert-primary');
+    alert.textContent = "A simple primary alert—check it out!";
+});
+
+// task 2 
+
+let btn1 = document.querySelector('.btn-secondary');
+btn1.addEventListener('click', () => {
+    alert.classList.add('alert-primary');
+    alert.textContent = "A simple secondary alert—check it out!";
+})
+
+// task 3
+
+let btn2 = document.querySelector('.btn-success');
+btn2.addEventListener('mouseover', () => {
+    alert.classList.add('alert-success');
+    alert.textContent = "A simple success alert—check it out!";   
+})
+btn2.addEventListener('mouseout', () => {
+    alert.classList.remove('alert-success');
+    alert.textContent = "";
+});
+
+// task 4
+
+let btn3 = document.querySelector('.btn-danger');
+btn3.addEventListener('focus', () => {
+    alert.classList.add('alert-danger');
+    alert.textContent = "A simple danger alert—check it out!";   
+})
+btn3.addEventListener('focusout', () => {
+    alert.classList.remove('alert-danger');
+    alert.textContent = "";
+});
+
+// task 5
+
+function toggleMode() {
+    let btnDark = document.querySelector('.btn-dark');
+    let btnLight = document.querySelector('.btn-light');
+
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+
+        btnDark.style.display = 'none';
+        btnLight.style.display = 'inline-block';
+    } else {
+        btnDark.style.display = 'inline-block';
+        btnLight.style.display = 'none';
+    }
+}
+document.querySelector('.btn-dark').addEventListener('click', toggleMode);
+document.querySelector('.btn-light').addEventListener('click', toggleMode);
+
+// task 6
+
+let btnInfo = document.querySelector('.btn-info');
+
+btnInfo.addEventListener('keypress', function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        alert.classList.add('alert-info');
+        alert.textContent = "A simple info alert—check it out!";
+    }
+});
+
+// task 7
+
+let cards = document.querySelectorAll('.card');
+
+for (let i = 0; i < cards.length; i++) {
+    let cardTitle = cards[i].querySelector('.card-title');
+    if (cardTitle) {
+        console.log(cardTitle.textContent);
+    }
+}
+
+// task 8
+
+{
+let cards = document.querySelectorAll('.card');
+
+for (let i = 0; i < cards.length; i++) {
+    let addToCartButton = cards[i].querySelector('.add-to-cart');
+    addToCartButton.addEventListener('click', () => {
+        let title = cards[i].querySelector('.card-title').textContent;
+        console.log(title);
+        });
+    }
+}
